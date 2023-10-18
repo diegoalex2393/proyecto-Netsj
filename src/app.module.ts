7a0f1107ab475bdf53ddc55ad8ff36ac9bb55a1e
Jaret <Jaretsantos01@gmail.com>
2023-10-18 16:14:22 -0500

    cambio 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { AuthModule } from './auth/auth.module';
+import { CustomersModule } from './customers/customers.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CatsModule, AuthModule, CustomersModule, InventoryModule, OrdersModule, PaymentsModule, ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
